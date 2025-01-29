7c5c3078e9b4f4225878dfab37a4898c2fb87780
arbazMasiro <masiroarbaz@gmail.com>
2025-01-29 22:14:13 +0530

    a

--- src/main/java/com/Tayyab/Employee/controller/EmployeeController.java
+++ src/main/java/com/Tayyab/Employee/controller/EmployeeController.java
@@ -1,15 +1,13 @@
 package com.Tayyab.Employee.controller;
 
-import com.Tayyab.Employee.exception.EmployeeNotFound;
 import com.Tayyab.Employee.model.Employee;
 import com.Tayyab.Employee.request.EmployeeRequest;
 import com.Tayyab.Employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.lang.annotation.Retention;
+
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
@@ -18,67 +16,23 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @PostMapping("/")
-    public ResponseEntity<?> registerEmployee(@RequestBody EmployeeRequest employeeRequest) {
-        Employee employee = employeeService.createEmployee(employeeRequest);
-        return new ResponseEntity<>(employee, HttpStatus.CREATED);
+    public void postEmployee(@RequestBody EmployeeRequest employeeRequest) {
+        employeeService.createEmployee(employeeRequest);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getEmployeeById(@PathVariable Long id) {
-        Employee employee = employeeService.findEmployeeById(id);
-        if (employee == null) {
-            throw new EmployeeNotFound("Employee not found!");
-        }
-        return ResponseEntity.status(HttpStatus.OK)
-                .body("userFound Successfully");
+    @GetMapping("/findById")
+    public Employee getEmployee(@PathVariable Long employeeId) {
+        return employeeService.findEmployeeById(employeeId);
     }
 
-    @GetMapping
-    public ResponseEntity<?> getAllEmployee() {
-        List<Employee> allEmployee = employeeService.findAllEmployee();
-        if (allEmployee != null && !allEmployee.isEmpty()) {
-            return new ResponseEntity<>(allEmployee, HttpStatus.OK);
-        }
-        throw new EmployeeNotFound("Employee not Found");
+    @DeleteMapping("/deleteById")
+    public void deleteEmployee(@PathVariable Long employeeId) {
+        employeeService.deleteById(employeeId);
     }
 
-    @DeleteMapping
-    public ResponseEntity<?> deleteAllEmployee() {
-        List<Employee> deletedlist = employeeService.deleteAllEmployee();
-        if (deletedlist != null && !deletedlist.isEmpty()) {
-            return new ResponseEntity<>(deletedlist, HttpStatus.OK);
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Now the Database is Empty");
+    @PutMapping("/")
+    public String updateEmployee(@RequestBody EmployeeRequest employeeRequest, @PathVariable Long employeeId) {
+        employeeService.updateEmployee(employeeId, employeeRequest);
+        return "employee updated successfully";
     }
-
-    @PutMapping("/{myId}")
-    public String updateEmployeeById(@RequestBody EmployeeRequest employeeRequest, @PathVariable Long myId) {
-        employeeService.updateEmployeeById(myId, employeeRequest);
-        return "Employee Updated Successfully";
-    }
-//
-//    @PostMapping("/")
-//    public void postEmployee(@RequestBody EmployeeRequest employeeRequest) {
-//        employeeService.createEmployee(employeeRequest);
-//    }
-//
-//    @GetMapping("/findById")
-//    public Employee getEmployee(@PathVariable Long employeeId) {
-//        return employeeService.findEmployeeById(employeeId);
-//    }
-//
-//    @DeleteMapping("/{employeeId}")
-//    public void deleteEmployee(@PathVariable Long employeeId) {
-//        employeeService.deleteById(employeeId);
-//    }
-//
-//    @PutMapping("/{employeeId")
-//    public String updateEmployee(@RequestBody EmployeeRequest employeeRequest, @PathVariable Long employeeId) {
-//        employeeService.updateEmployeeById(employeeId, employeeRequest);
-//        return "employee updated successfully";
-//    }
 }
-
-
-
-
